51d3bc3a17351e00da928a9c18649dcb8a98793a
metafates <vvz.dev@gmail.com>
2024-10-08 22:36:45 +0300

    refactor

--- swiftty_backend/src/action.rs
+++ swiftty_backend/src/action.rs
@@ -1,13 +1,33 @@
+use std::{
+    char,
+    cmp::{max, min},
+};
+
+use swiftty_vte::executor::Executor;
+
+use crate::utf;
+
+#[derive(Debug, PartialEq)]
 enum Action {
     Print(char),
     Execute(u8),
     Put(u8),
     Unhook,
+    CsiDispatch {
+        params: Vec<Vec<u16>>,
+        intermediates: Vec<u8>,
+        ignore: bool,
+        action: char,
+    },
 }
 
 #[derive(Default)]
 struct Dispatcher {
     dispatched: Vec<Action>,
+
+    utf8_bytes: [u8; 4],
+    utf8_len: usize,
+    utf8_remaining_count: usize,
 }
 
 impl Dispatcher {
@@ -37,6 +57,10 @@ impl Dispatcher {
 
             self.process_utf8(&remaining_bytes[..next_sequence_start]);
 
+            if self.utf8_remaining_count > 0 {
+                self.consume_utf8();
+            }
+
             remaining_bytes = &remaining_bytes[next_sequence_start..];
 
             let mut i = 0;
@@ -45,7 +69,7 @@ impl Dispatcher {
                 parser.advance(self, remaining_bytes[i]);
                 i += 1;
 
-                if parser.in_escape_sequence() && i < remaining_bytes.len() {
+                if !(parser.in_escape_sequence() && i < remaining_bytes.len()) {
                     break;
                 }
             }
@@ -55,11 +79,46 @@ impl Dispatcher {
     }
 
     fn process_utf8(&mut self, bytes: &[u8]) {
-        todo!()
+        let mut remaining_bytes = bytes;
+
+        while !remaining_bytes.is_empty() {
+            let want_bytes_count = {
+                if self.utf8_remaining_count > 0 {
+                    self.utf8_remaining_count
+                } else if let Some(count) = utf::expected_utf8_bytes_count(remaining_bytes[0]) {
+                    count
+                } else {
+                    1
+                }
+            };
+
+            let bytes_count = min(want_bytes_count, remaining_bytes.len());
+
+            for i in 0..bytes_count {
+                self.utf8_bytes[self.utf8_len] = remaining_bytes[i];
+                self.utf8_len += 1;
+            }
+
+            self.utf8_remaining_count = want_bytes_count - bytes_count;
+
+            if self.utf8_remaining_count == 0 {
+                self.consume_utf8();
+            }
+
+            remaining_bytes = &remaining_bytes[bytes_count..];
+        }
+    }
+
+    fn consume_utf8(&mut self) {
+        let ch = utf::char_from_utf8(&self.utf8_bytes[..self.utf8_len]);
+        self.print(ch);
+
+        self.utf8_len = 0;
+        self.utf8_remaining_count = 0;
     }
 }
 
-impl swiftty_vte::executor::Executor for Dispatcher {
+impl Executor for Dispatcher {
     fn print(&mut self, c: char) {
         self.dispatched.push(Action::Print(c));
     }
@@ -101,7 +160,15 @@ impl swiftty_vte::executor::Executor for Dispatcher {
         ignore: bool,
         action: char,
     ) {
-        todo!()
+        self.dispatched.push(Action::CsiDispatch {
+            params: params
+                .into_iter()
+                .map(|p| p.into_iter().collect())
+                .collect(),
+            intermediates: intermediates.into_iter().map(|b| *b).collect(),
+            ignore,
+            action,
+        });
     }
 }
 
@@ -127,5 +194,31 @@ mod tests {
         let mut dispatcher = Dispatcher::new();
 
         dispatcher.process(&mut parser, b"hello\x1b[38:2:255:0:255;1m");
+        dispatcher.process(&mut parser, &[0xD0]);
+        dispatcher.process(&mut parser, &[0x96]);
+        dispatcher.process(&mut parser, &[0xE6, 0xBC, 0xA2]);
+        dispatcher.process(&mut parser, &[0xE6, 0xBC, 0x1B]); // abort utf8 sequence
+
+        let actions = dispatcher.take_dispatched_actions();
+
+        assert_eq!(
+            actions,
+            vec![
+                Action::Print('h'),
+                Action::Print('e'),
+                Action::Print('l'),
+                Action::Print('l'),
+                Action::Print('o'),
+                Action::CsiDispatch {
+                    params: vec![vec![38, 2, 255, 0, 255], vec![1]],
+                    intermediates: vec![],
+                    ignore: false,
+                    action: 'm',
+                },
+                Action::Print('Ж'),
+                Action::Print('漢'),
+                Action::Print(char::REPLACEMENT_CHARACTER),
+            ]
+        );
     }
 }
--- swiftty_backend/src/utf.rs
+++ swiftty_backend/src/utf.rs
@@ -11,28 +11,20 @@ pub fn find_utf8_start(bytes: &[u8]) -> Option<usize> {
     None
 }
 
-#[derive(Clone, Copy, Debug)]
-pub enum UTF8BytesCount {
-    One = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-}
-
-pub fn expected_utf8_bytes_count(first_byte: u8) -> Option<UTF8BytesCount> {
-    use UTF8BytesCount::*;
-
+pub fn expected_utf8_bytes_count(first_byte: u8) -> Option<usize> {
+    // TODO: make it branchless
     match first_byte {
-        0x0..=0x7F => Some(One),
-        0xC2..=0xDF => Some(Two),
-        0xE0..=0xEF => Some(Three),
-        0xF0..=0xF4 => Some(Four),
+        0x0..=0x7F => Some(1),
+        0xC2..=0xDF => Some(2),
+        0xE0..=0xEF => Some(3),
+        0xF0..=0xF4 => Some(4),
         _ => None,
     }
 }
 
 pub fn char_from_utf8(utf8: &[u8]) -> char {
-    let s = std::str::from_utf8(utf8).expect("Invalid UTF-8 sequence");
-
-    s.chars().next().expect("No character found")
+    match simdutf8::basic::from_utf8(utf8) {
+        Ok(s) => s.chars().next().expect("No character found"),
+        Err(_) => char::REPLACEMENT_CHARACTER,
+    }
 }
