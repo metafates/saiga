0bc4e4e4cd00cf21099a2ff3263910d4a542eccb
metafates <vvz.dev@gmail.com>
2024-10-24 21:49:31 +0300

    simplify for now

--- saiga_backend/src/lib.rs
+++ saiga_backend/src/lib.rs
@@ -1,8 +1,8 @@
 use std::mem;
 
-use event::{Event, EventListener};
-use grid::{cell::UnderlineType, Dimensions, Grid};
-use saiga_vte::ansi::handler::{Attribute, Handler, NamedMode, NamedPrivateMode, PrivateMode};
+use event::EventListener;
+use grid::{Dimensions, Grid};
+use saiga_vte::ansi::handler::{Attribute, Handler, PrivateMode};
 use unicode_width::UnicodeWidthChar;
 
 pub mod event;
@@ -53,123 +53,6 @@ impl<E: EventListener> Terminal<E> {
 }
 
 impl<E: EventListener> Handler for Terminal<E> {
-    fn set_title(&mut self, title: &str) {
-        self.event_listener
-            .on_event(Event::SetTitle(title.to_string()));
-    }
-
-    fn set_cursor_shape(&mut self, shape: saiga_vte::ansi::handler::CursorShape) {
-        todo!()
-    }
-
-    fn set_cursor_position(&mut self, position: saiga_vte::ansi::handler::Position) {
-        self.grid.cursor.position.line = position.line;
-        self.grid.cursor.position.column = position.column;
-    }
-
-    fn set_cursor_line(&mut self, line: saiga_vte::ansi::handler::Line) {
-        self.grid.cursor.position.line = line;
-    }
-
-    fn set_cursor_column(&mut self, column: saiga_vte::ansi::handler::Column) {
-        self.grid.cursor.position.column = column;
-    }
-
-    fn set_charset(&mut self, charset: saiga_vte::ansi::handler::CharsetIndex) {
-        todo!()
-    }
-
-    fn set_clipboard(&mut self, clipboard: u8, payload: &[u8]) {
-        todo!()
-    }
-
-    fn set_mode(&mut self, mode: saiga_vte::ansi::handler::Mode) {
-        use saiga_vte::ansi::handler::Mode as HandlerMode;
-
-        let HandlerMode::Named(mode) = mode else {
-            return;
-        };
-
-        match mode {
-            NamedMode::Insert => self.mode.insert = true,
-            NamedMode::LineFeedNewLine => self.mode.line_feed_new_line = true,
-        }
-    }
-
-    fn set_private_mode(&mut self, mode: saiga_vte::ansi::handler::PrivateMode) {
-        let PrivateMode::Named(mode) = mode else {
-            return;
-        };
-
-        match mode {
-            NamedPrivateMode::CursorKeys => todo!(),
-            NamedPrivateMode::ColumnMode => todo!(),
-            NamedPrivateMode::Origin => todo!(),
-            NamedPrivateMode::LineWrap => todo!(),
-            NamedPrivateMode::BlinkingCursor => todo!(),
-            NamedPrivateMode::ShowCursor => todo!(),
-            NamedPrivateMode::ReportMouseClicks => todo!(),
-            NamedPrivateMode::ReportCellMouseMotion => todo!(),
-            NamedPrivateMode::ReportAllMouseMotion => todo!(),
-            NamedPrivateMode::ReportFocusInOut => todo!(),
-            NamedPrivateMode::Utf8Mouse => todo!(),
-            NamedPrivateMode::SgrMouse => todo!(),
-            NamedPrivateMode::AlternateScroll => todo!(),
-            NamedPrivateMode::UrgencyHints => todo!(),
-            NamedPrivateMode::SwapScreenAndSetRestoreCursor => todo!(),
-            NamedPrivateMode::BracketedPaste => todo!(),
-            NamedPrivateMode::SyncUpdate => todo!(),
-        }
-    }
-
-    fn set_attribute(&mut self, attribute: saiga_vte::ansi::handler::Attribute) {
-        let template = &mut self.grid.cursor.template;
-
-        match attribute {
-            Attribute::Reset => template.reset_template(),
-            Attribute::Bold => template.bold = true,
-            Attribute::Dim => template.dim = true,
-            Attribute::Italic => template.italic = true,
-            Attribute::Underline => template.underline_type = Some(UnderlineType::Regular),
-            Attribute::DoubleUnderline => template.underline_type = Some(UnderlineType::Double),
-            Attribute::Undercurl => template.underline_type = Some(UnderlineType::Curl),
-            Attribute::DottedUnderline => template.underline_type = Some(UnderlineType::Dotted),
-            Attribute::DashedUnderline => template.underline_type = Some(UnderlineType::Dashed),
-            Attribute::BlinkSlow => todo!(),
-            Attribute::BlinkFast => todo!(),
-            Attribute::Reverse => template.reverse = true,
-            Attribute::Hidden => todo!(),
-            Attribute::Strike => todo!(),
-            Attribute::CancelBold => template.bold = false,
-            Attribute::CancelBoldDim => {
-                template.bold = false;
-                template.dim = false;
-            }
-            Attribute::CancelItalic => template.italic = false,
-            Attribute::CancelUnderline => template.underline_type = None,
-            Attribute::CancelBlink => todo!(),
-            Attribute::CancelReverse => template.reverse = false,
-            Attribute::CancelHidden => todo!(),
-            Attribute::CancelStrike => todo!(),
-            Attribute::Foreground(color) => template.foreground = color,
-            Attribute::Background(color) => template.background = color,
-            Attribute::UnderlineColor(color) => {
-                // TODO: do we need to reset the color if it's None?
-                if let Some(color) = color {
-                    template.underline_color = color
-                }
-            }
-        }
-    }
-
-    fn reset_state(&mut self) {
-        if self.mode.alterantive_screen {
-            self.swap_grids();
-        }
-
-        self.mode = TerminalMode::default();
-    }
-
     fn move_cursor(
         &mut self,
         direction: saiga_vte::ansi::handler::Direction,
@@ -236,6 +119,71 @@ impl<E: EventListener> Handler for Terminal<E> {
         }
     }
 
+    fn newline(&mut self) {
+        self.linefeed();
+
+        if self.mode.line_feed_new_line {
+            self.carriage_return();
+        }
+    }
+
+    fn carriage_return(&mut self) {
+        self.grid.cursor.position.column = 0;
+    }
+
+    fn linefeed(&mut self) {
+        let next_line = self.grid.cursor.position.line + 1;
+
+        // TODO
+        if next_line >= self.grid.height() {
+            self.grid.cursor.position.line = next_line;
+        }
+    }
+
+    fn set_title(&mut self, title: &str) {
+        todo!()
+    }
+
+    fn set_cursor_shape(&mut self, shape: saiga_vte::ansi::handler::CursorShape) {
+        todo!()
+    }
+
+    fn set_cursor_position(&mut self, position: saiga_vte::ansi::handler::Position) {
+        todo!()
+    }
+
+    fn set_cursor_line(&mut self, line: saiga_vte::ansi::handler::Line) {
+        todo!()
+    }
+
+    fn set_cursor_column(&mut self, column: saiga_vte::ansi::handler::Column) {
+        todo!()
+    }
+
+    fn set_charset(&mut self, charset: saiga_vte::ansi::handler::CharsetIndex) {
+        todo!()
+    }
+
+    fn set_clipboard(&mut self, clipboard: u8, payload: &[u8]) {
+        todo!()
+    }
+
+    fn set_mode(&mut self, mode: saiga_vte::ansi::handler::Mode) {
+        todo!()
+    }
+
+    fn set_private_mode(&mut self, mode: PrivateMode) {
+        todo!()
+    }
+
+    fn set_attribute(&mut self, attribute: Attribute) {
+        todo!()
+    }
+
+    fn reset_state(&mut self) {
+        todo!()
+    }
+
     fn put_tab(&mut self) {
         todo!()
     }
@@ -269,15 +217,11 @@ impl<E: EventListener> Handler for Terminal<E> {
     }
 
     fn save_cursor_position(&mut self) {
-        self.grid.saved_cursor = Some(self.grid.cursor.clone());
+        todo!()
     }
 
     fn restore_cursor_position(&mut self) {
-        let Some(saved_cursor) = mem::take(&mut self.grid.saved_cursor) else {
-            return;
-        };
-
-        self.grid.cursor = saved_cursor;
+        todo!()
     }
 
     fn delete_lines(&mut self, count: usize) {
@@ -292,37 +236,12 @@ impl<E: EventListener> Handler for Terminal<E> {
         todo!()
     }
 
-    fn newline(&mut self) {
-        self.linefeed();
-
-        if self.mode.line_feed_new_line {
-            self.carriage_return();
-        }
-    }
-
-    fn carriage_return(&mut self) {
-        self.grid.cursor.position.column = 0;
-    }
-
     fn ring_bell(&mut self) {
         todo!()
     }
 
     fn backspace(&mut self) {
-        if self.grid.cursor.position.column == 0 {
-            return;
-        }
-
-        self.grid.cursor.position.column -= 1;
-    }
-
-    fn linefeed(&mut self) {
-        let next_line = self.grid.cursor.position.line + 1;
-
-        // TODO
-        if next_line >= self.grid.height() {
-            self.grid.cursor.position.line = next_line;
-        }
+        todo!()
     }
 
     fn substitute(&mut self) {
